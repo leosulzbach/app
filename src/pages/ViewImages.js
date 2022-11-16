418aebe26b48f9e18287163dba047b57070912dd
leonardo.sulzbach1 <leonardo.sulzbach1@universo.univates.br>
2022-11-16 17:24:17 +0000

    att

--- src/pages/ViewImages.js
+++ src/pages/ViewImages.js
@@ -3,23 +3,17 @@ import {SafeAreaView, View} from 'react-native';
 
 export const ViewImages = () => {
     return (
-        <View style={{ flex: 1}}>
+        <View style={{flex: 1, justifyContent: 'center'}}>
             <SafeAreaView>
-                <Text>Tela de Imagens</Text>
-
                 <Image 
-                    source = {require('../../assets/favicon.png')}>
+                    resizeMode="contain"
+                    style={{width: 200, height: 200}}
+                    source = {{
+                        uri: 'https://static.vecteezy.com/system/resources/previews/001/191/989/non_2x/circle-logo-png.png',
+                    }}>
                 </Image>
-            </SafeAreaView>
+            </SafeAreaView>    
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        width: 50,
-        height: 50
-    }
-})
